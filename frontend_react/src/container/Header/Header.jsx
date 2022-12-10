bd80557246426ed9c32557e5d8171f81aa9e9381
Asraf2asif <asraf2asif@gmail.com>
2022-12-10 20:14:52 +0600

    update home animation

--- frontend_react/src/container/Header/Header.jsx
+++ frontend_react/src/container/Header/Header.jsx
@@ -1,8 +1,8 @@
-import React from 'react';
-import './Header.scss';
-import { MdWavingHand } from 'react-icons/md';
-import { motion } from 'framer-motion';
-import { images, variables } from '../../constants';
+import React from "react";
+import "./Header.scss";
+import { MdWavingHand } from "react-icons/md";
+import { motion } from "framer-motion";
+import { images, variables } from "../../constants";
 
 const scaleVariants = {
   whileInView: {
@@ -10,7 +10,7 @@ const scaleVariants = {
     opacity: [0, 1],
     transition: {
       duration: 1,
-      ease: 'easeInOut',
+      ease: "easeInOut",
     },
   },
 };
@@ -23,26 +23,25 @@ const Header = (props) => {
     <header {...props} style={{ backgroundImage: `url(${bgImg})` }}>
       <motion.div
         whileInView={{
-          x: [-100, 0],
-          opacity: [0, 1],
+          x: [-50, 0],
         }}
         transition={{ duration: 0.5 }}
-        className='app__header-info'
+        className="app__header-info"
       >
-        <div className='badge'>
-          <div className='badge-cmp app__flex-center'>
+        <div className="badge">
+          <div className="badge-cmp app__flex-center">
             <span>
               <MdWavingHand />
             </span>
             <div>
-              <p className='p-text'>Hello, I am</p>
-              <p className='name'>Asif</p>
+              <p className="p-text">Hello, I am</p>
+              <p className="name">Asif</p>
             </div>
           </div>
 
-          <div className='tag-cmp app__flex-center'>
-            <p className='p-text'>Web Developer</p>
-            <p className='p-text'>UI/UX Designer</p>
+          <div className="tag-cmp app__flex-center">
+            <p className="p-text">Web Developer</p>
+            <p className="p-text">UI/UX Designer</p>
           </div>
         </div>
       </motion.div>
@@ -51,28 +50,28 @@ const Header = (props) => {
         whileInView={{
           opacity: [0, 1],
         }}
-        transition={{ duration: 0.5, delayChildren: 0.5 }}
-        className='app__header-img'
+        transition={{ duration: 0.2, delayChildren: 0.5 }}
+        className="app__header-img"
       >
         <div
-          className='img'
+          className="img"
           style={{ backgroundImage: `url(${profileImg})` }}
         />
         <motion.img
           whileInView={{
             scale: [0, 1],
           }}
-          transition={{ duration: 1, ease: 'easeInOut' }}
+          transition={{ duration: 0.5, ease: "easeInOut" }}
           src={circleImg}
-          alt='profile_circle'
-          className='overlay_circle'
+          alt="profile_circle"
+          className="overlay_circle"
         />
         <motion.div
           whileInView={{
-            opacity: [0, 1],
+            y: [50, 0],
           }}
-          transition={{ duration: 0.5, ease: 'easeInOut' }}
-          className='bio'
+          transition={{ duration: 0.5, ease: "easeInOut" }}
+          className="bio"
         >
           A programmer with a wide range of
           <br />
@@ -84,14 +83,14 @@ const Header = (props) => {
       <motion.div
         variants={scaleVariants}
         whileInView={scaleVariants.whileInView}
-        className='app__header-circles'
+        className="app__header-circles"
       >
-        {headCircleList.map(({ name = '', icon = '' }, idx) => (
-          <div className='circle-cmp app__flex-center' key={`circle-${idx}`}>
+        {headCircleList.map(({ name = "", icon = "" }, idx) => (
+          <div className="circle-cmp app__flex-center" key={`circle-${idx}`}>
             <img src={icon} alt={`circle-${icon}`} />
-            <div className='badge app__flex-center'>
+            <div className="badge app__flex-center">
               <div>
-                <p className='p-text'>{name}</p>
+                <p className="p-text">{name}</p>
               </div>
             </div>
           </div>
