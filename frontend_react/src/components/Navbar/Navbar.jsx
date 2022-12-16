419e79483e6ee5a49de9f6fe621d63d557d0a431
Asraf2asif <asraf2asif@gmail.com>
2022-12-16 22:59:06 +0600

    add resume button on nav

--- frontend_react/src/components/Navbar/Navbar.jsx
+++ frontend_react/src/components/Navbar/Navbar.jsx
@@ -5,7 +5,7 @@ import { HiX } from "react-icons/hi";
 import { FaBars } from "react-icons/fa";
 import { motion } from "framer-motion";
 
-const { menuList } = variables;
+const { menuList, menuIconList, resumeLink } = variables;
 const { logo: logoImg } = images;
 
 const Navbar = () => {
@@ -21,9 +21,23 @@ const Navbar = () => {
         {menuList.map((item) => (
           <li className="app__flex-center p-text" key={`link-${item}`}>
             <div />
-            <a href={`#${item}`}>{item}</a>
+            <a href={`#${item}`}>
+              {!menuIconList[item] ? null : typeof menuIconList[item] ===
+                "string" ? (
+                <i className={menuIconList[item]} />
+              ) : (
+                menuIconList[item]
+              )}
+              {item}
+            </a>
           </li>
         ))}
+        <li className="app__flex-center p-text resume">
+          <div />
+          <a href={resumeLink} target="_blank" rel="noreferrer">
+            Resume
+          </a>
+        </li>
       </ul>
 
       <div className="app__navbar-small">
@@ -45,6 +59,11 @@ const Navbar = () => {
                   </a>
                 </li>
               ))}
+              <li className="resume">
+                <a href={resumeLink} target="_blank" rel="noreferrer">
+                  Resume
+                </a>
+              </li>
             </ul>
           </motion.div>
         )}
